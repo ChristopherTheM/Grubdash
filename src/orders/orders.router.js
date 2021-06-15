70843c6c13d7f3300c0f4d635d0971da15e2d6db
Christopher Maurillo <69815717+ChristopherTheM@users.noreply.github.com>
2021-06-15 15:43:03 -0600

    Update orders.router.js

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,8 +1,11 @@
+//imports required to run functions at certain URL endpoints
+
 const router = require("express").Router();
 const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Implement the /orders routes needed to make the tests pass
 
+//all CRUD operations that need to be performed at the URL endpoint /orders/:orderId
 router
   .route("/:orderId")
   .get(controller.read)
@@ -10,6 +13,7 @@ router
   .delete(controller.delete)
   .all(methodNotAllowed);
 
+//all CRUD operations performed at the URL endpoint /orders
 router
   .route("/")
   .get(controller.list)
