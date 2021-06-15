189acf71e96823a01ad4cd84f5b1c12eadc42d79
Christopher Maurillo <69815717+ChristopherTheM@users.noreply.github.com>
2021-06-15 15:29:22 -0600

    Update errorHandler.js

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -1,3 +1,5 @@
+//This file is used for all errors not caught by methodNotAllowed or a URL that is not found
+
 function errorHandler(error, request, response, next) {
   // console.error(error);  // Commented out to silence tests.
   const { status = 500, message = "Something went wrong!" } = error;
