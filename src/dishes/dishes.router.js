73ec88c95d7ab960abfbe28c0977a9a3599aaff8
Christopher Maurillo <69815717+ChristopherTheM@users.noreply.github.com>
2021-06-15 15:37:18 -0600

    Update dishes.router.js

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,15 +1,19 @@
+//List of imports required
+
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
+//Routes for /:dishId when a specific ID is passed into the URL
 router
   .route("/:dishId")
   .get(controller.read)
   .put(controller.update)
   .all(methodNotAllowed);
 
+//routes for URL ending in /dishes
 router
   .route("/")
   .get(controller.list)
