cc332953b5bb07ef15bd621264877ca89222358c
Christopher Maurillo <69815717+ChristopherTheM@users.noreply.github.com>
2021-06-15 15:30:06 -0600

    Update notFound.js

--- src/errors/notFound.js
+++ src/errors/notFound.js
@@ -1,3 +1,5 @@
+//Error handler for a URL that isn't defined or not found
+
 function notFound(request, response, next) {
   next({ status: 404, message: `Path not found: ${request.originalUrl}` });
 }
